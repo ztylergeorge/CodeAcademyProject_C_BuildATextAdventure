64ffe8e942724cc634a280287c17c5940c793354
ztylergeorge <ztylergeorge@gmail.com>
2021-03-20 13:37:14 -0400

    creation of initial prompt

---
+++ build_a_text_adventure.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+
+    //give initial prompt
+    std::string name;
+    int district;
+    std::cout << "Welcome to the Hunger Games of 2021! What is your name?\n";
+    std::cin >> name;
+    std::cout << "Well, " << name << ", what district are you from? 1, 2,...12?\n";
+    std::cin >> district;
+    std::cout << "Welcome " << name << " of District " << district << "!\nMay the odds be ever in your favor.\n";
+
+    
+
+
+
+
+
+
+
+}
